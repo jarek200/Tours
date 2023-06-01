58663b284c2989dd25b5680b8b6c274c28ddb146
Jarek Wyprzal <jwyprzal@gmail.com>
2023-06-01 22:35:43 +0100

    add read more button

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
 export default function Tour({ id, name, info, image, price, removeTour }) {
+  const [readMore, setReadMore] = useState(false)
+
   return (
     <>
       <article className='single-tour single-tour-hover'>
@@ -6,7 +9,13 @@ export default function Tour({ id, name, info, image, price, removeTour }) {
         <span className='tour-price'>${price}</span>
         <div className='tour-info'>
           <h5>{name}</h5>
-          <p>{info}</p>
+          <p>
+            {readMore ? info : info.substring(0, 200)} ...
+            <button className='info-btn' onClick={() => setReadMore(!readMore)}>
+              Read More
+            </button>{' '}
+          </p>
+
           <button className='btn btn-block delete-btn' onClick={() => removeTour(id)}>
             Remove tour
           </button>
